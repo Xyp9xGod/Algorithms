b05e9ae721dec6db5932a0e8e0e97aa8234c06b9
usuario <usuario@HOME_PC>
2024-09-16 20:46:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿List<int> list = new List<int> { 8, 6, 3, 9, 0, 1, 7, 2, 4, 5 };
+
+SelectionSort(list);
+
+// Imprimir a lista ordenada
+Console.WriteLine("Lista ordenada: " + string.Join(", ", list));
+
+static List<int> SelectionSort(List<int> list)
+{
+    var minIndex = 0;
+
+    for (int currentIndex = 0; currentIndex <= list.Count - 1; currentIndex++)
+    {
+        minIndex = currentIndex;
+
+        for (int searchIndex = currentIndex; searchIndex < list.Count; searchIndex++)
+        {
+            if (list[searchIndex] < list[minIndex])
+            {
+                minIndex = searchIndex;
+            }
+        }
+
+        if (list[currentIndex] > list[minIndex])
+        {
+            var aux = list[currentIndex];
+            list[currentIndex] = list[minIndex];
+            list[minIndex] = aux;
+        }
+    }
+
+    return list;
+}
